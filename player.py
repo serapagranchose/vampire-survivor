22e713fcae0ac6695e598b11bbc110500c8aab2a
EliotMartinJag <eliot.martin@jagger-lewis.com>
2024-01-20 15:36:10 +0100

    feat: enemy follow player + take damages

--- player.py
+++ player.py
@@ -5,16 +5,8 @@ from globals import *
 class Player :
     def __init__(self, entity):
         self.entity = entity
+        self.color = ((255, 0, 0))
 
-    def move(self, key) :
-        if key[pygame.K_z] == True or key[pygame.K_UP] == True:
-            self.entity.move_ip(0, 0 if self.entity.y - 1 <= 400 else -1 )
-        if key[pygame.K_s] == True or key[pygame.K_DOWN] == True:
-            self.entity.move_ip(0, 0 if self.entity.y + 1 >= SCREEN_HEIGHT - PLAYER_HEIGHT else 1)
-        if key[pygame.K_q] == True or key[pygame.K_LEFT] == True:
-            self.entity.move_ip(0 if self.entity.x - 1 <= 0 else -1, 0)
-        if key[pygame.K_d] == True  or key[pygame.K_RIGHT] == True:
-            self.entity.move_ip(0 if self.entity.x + 1 >= ZONE_WIDTH - PLAYER_WIDTH else 1, 0)
 
     def check_col(self, obstacle):
         if  ((
