b11a5bcd1f563c9f42b26f6e2666cb8e17685f17
serapagranchose <seraphin.perrot@epitech.eu>
2024-01-20 13:22:02 +0100

    adding character animations, directions

--- game.py
+++ game.py
@@ -3,10 +3,39 @@ from asyncio import sleep
 from obstacle import Obstacle
 from player import Player
 import pygame
+from os import listdir
+from os.path import isfile, join
 
 pygame.init()
 
 score = 0
+
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+    
+def load_sprite_sheets(dir1, dir2, width, height, direction = False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+            
+        if direction:
+            all_sprites[image.replace('.png', '') + '_right'] = sprites
+            all_sprites[image.replace('.png', '') + '_left'] = flip(sprites)
+        else:
+            all_sprites[image.replace('.png', '')] = sprites
+            
+    return all_sprites
 
 def clean(score):
     for ob in obs:
@@ -19,7 +48,9 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(f'Score : ')
 clock = pygame.time.Clock()
 
-player = Player(pygame.Rect((400, 500, PLAYER_WIDTH, PLAYER_HEIGHT)))
+sprite_sheet_image = pygame.image.load('assets/characters/MiniNobleMan.png').convert_alpha()
+
+player = Player(400, 500, 250, 250, pygame.Rect((400, 500, PLAYER_WIDTH, PLAYER_HEIGHT)), sprite_sheet_image, load_sprite_sheets, flip)
 space = pygame.Rect((0, 400, ZONE_WIDTH, ZONE_HEIGHT))
 font = pygame.font.SysFont('inkfree',30,italic=True,bold=True)#try inkfree, georgia,impact,dubai,arial
 
@@ -29,13 +60,12 @@ textrect = text.get_rect()
 tmp = pygame.Rect((50, SCREEN_HEIGHT - 125, 50, 50))
 pygame.Rect((400, 500, PLAYER_WIDTH, PLAYER_HEIGHT)).bottomleft
 
-
 run = True
 tick = 0
 obs = []
 
 while run :
-    screen.fill((0,0,0))
+    screen.fill((50, 50, 50))
     text = font.render(f'Score : {score}',True,(255,255,255))
 
     screen.blit(text, textrect)
@@ -56,7 +86,7 @@ while run :
     for ob in obs : 
         pygame.draw.rect(screen, ob.color, ob.entity)
     
-    pygame.draw.rect(screen, (255, 0, 0), player.entity)
+    player.draw(screen)
 
     for event in pygame.event.get() : 
         if event.type == pygame.QUIT :
@@ -69,4 +99,3 @@ while run :
 
 print(score)
 pygame.quit()
-
--- player.py
+++ player.py
@@ -1,20 +1,49 @@
 import pygame
 from globals import *
 
+class Player(pygame.sprite.Sprite) :
+    ANIMATION_DELAY = 5
 
-class Player :
-    def __init__(self, entity):
+    def __init__(self, x, y, width, height, entity, sheet, load_sprite_sheets, flip):
+        self.rect = pygame.Rect(x, y, width, height)
         self.entity = entity
+        self.sheet = sheet
+        self.direction = "left"
+        self.animation_count = 0
+        self.load_sprite_sheets = load_sprite_sheets
+        self.flip = flip
 
-    def move(self, key) :
+    def draw(self, win):
+        # self.sprite = self.load_sprite_sheets("characters", "MiniNobleMan", 32, 32, True)["idle_" + self.direction][0]
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
+
+    def move(self, key):
+        sprite_sheet = "idle"
         if key[pygame.K_z] == True or key[pygame.K_UP] == True:
+            sprite_sheet = "run"
             self.entity.move_ip(0, 0 if self.entity.y - 1 <= 400 else -1 )
         if key[pygame.K_s] == True or key[pygame.K_DOWN] == True:
+            sprite_sheet = "run"
             self.entity.move_ip(0, 0 if self.entity.y + 1 >= SCREEN_HEIGHT - PLAYER_HEIGHT else 1)
         if key[pygame.K_q] == True or key[pygame.K_LEFT] == True:
+            sprite_sheet = "run"
             self.entity.move_ip(0 if self.entity.x - 1 <= 0 else -1, 0)
+            if self.direction != 'left':
+                self.direction = 'left'
+                self.animation_count = 0
         if key[pygame.K_d] == True  or key[pygame.K_RIGHT] == True:
+            sprite_sheet = "run"
             self.entity.move_ip(0 if self.entity.x + 1 >= ZONE_WIDTH - PLAYER_WIDTH else 1, 0)
+            if self.direction != 'right':
+                self.direction = 'right'
+                self.animation_count = 0
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.load_sprite_sheets("characters", "MiniNobleMan", 32, 32, True)[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY % len(sprites))
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+
 
     def check_col(self, obstacle):
         if  ((
